9d44441aaf8dbc2c8131b8a374501966e7298c50
zhl146 <zhl146@gmail.com>
2016-03-04 22:31:05 -0500

    Changed where close database function is located.

--- rest_manager.py
+++ rest_manager.py
@@ -15,9 +15,12 @@ def get_question(task_id):
     try:
         dbconnect = db_access_layer.database_access()
         result = dbconnect.get_question(1)
+        dbconnect.close_connection()
         return "made a connection, getting read to check for id="+str(result)
     except Exception as ex:
         return "you passed in "+str(task_id)+ " exception give: "+str(ex)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
