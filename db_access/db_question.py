5b86edab809d5ef3ca5fe41861e5f056577aa446
zhl14 <zhl146@gmail.com>
2016-03-11 23:50:31 -0500

    you don't read these anyways, jerk.

--- db_access/db_question.py
+++ db_access/db_question.py
@@ -11,7 +11,7 @@ class DatabaseAccess:
         self.dbconnection = pymysql.connect(host='localhost',
                              user='appuser',
                              password='carhorsebatterysuccess',
-                             db='testDB',
+                             db='testdb',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor,
                              autocommit=True
@@ -28,13 +28,13 @@ class DatabaseAccess:
                         questionType = str(randint(0,2))
                         topic = "topic index " + str(randint(0,4))
 
-                        if questionType == '0':
-                            sql = "INSERT INTO testDB.questions (question_text, answer_a_text, topic , question_type) VALUES ('I am a definition blah blah blah blah blah blah blah blah blah my answer is " + answer + "', '" + answer + "', '" + topic + "', " + questionType + ");"
+                        #if questionType == '0':
+                        #    sql = "INSERT INTO testDB.questions (question_text, answer_a_text, topic , question_type) VALUES ('I am a definition blah blah blah blah blah blah blah blah blah my answer is " + answer + "', '" + answer + "', '" + topic + "', " + questionType + ");"
 
-                        else:
-                            sql = "INSERT INTO testDB.questions (question_text, answer_a_text, answer_b_text, answer_c_text, answer_d_text, answer_e_text, answer_f_text, answer_num, topic, question_type) VALUES ('I am a question. My answer is " + answer + ". My topic is  " + topic + ". My question type is " + questionType + "', 'one', 'two', 'three', 'four', 'five', 'six', " + answerID + ", '" + topic + "' , " + questionType + ");"
+                        #else:
+                        #    sql = "INSERT INTO testDB.questions (question_text, answer_a_text, answer_b_text, answer_c_text, answer_d_text, answer_e_text, answer_f_text, answer_num, topic, question_type) VALUES ('I am a question. My answer is " + answer + ". My topic is  " + topic + ". My question type is " + questionType + "', 'one', 'two', 'three', 'four', 'five', 'six', " + answerID + ", '" + topic + "' , " + questionType + ");"
 
-                        cursor.execute(sql)
+                        #cursor.execute(sql)
                     print("added " + str(numAdd) + " records to table")
 
             except Exception as e:
